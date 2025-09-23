a5c737dfafd46f25f197e5c0ec9a3abc0835e8b8
Anderson Castro <andersoncastb@gmail.com>
2025-09-22 19:14:07 -0500

    first commit

---
+++ src/test/java/co/com/soft/budget_manager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.soft.budget_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
